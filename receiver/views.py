70bf915e60ad4375c5f983bc95a16690eae598a8
AadeshLawange-Guardinger <aadesh.lawange@guardinger.in>
2024-03-21 11:50:28 +0530

    Data storage done.

--- receiver/views.py
+++ receiver/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from .models import RockBlockMessage
@@ -7,17 +8,26 @@ def receive_message(request):
     if request.method == 'POST':
         imei = request.POST.get('imei')
         momsn = request.POST.get('momsn')
-        transmit_time = request.POST.get('transmit_time')
+        transmit_time_str = request.POST.get('transmit_time')
         iridium_latitude = request.POST.get('iridium_latitude')
         iridium_longitude = request.POST.get('iridium_longitude')
         iridium_cep = request.POST.get('iridium_cep')
         data = request.POST.get('data')
 
+        try:
+            # Parse transmit_time string to datetime object
+            transmit_time = datetime.strptime(transmit_time_str, "%d-%m-%y %H:%M:%S")
+            # Format datetime object into the desired format
+            transmit_time_formatted = transmit_time.strftime("%Y-%m-%d %H:%M:%S")
+        except ValueError:
+            # If the format is incorrect, respond with an error
+            return JsonResponse({'error': 'Invalid datetime format'}, status=400)
+
         # Save the received message
         message = RockBlockMessage.objects.create(
             imei=imei,
             momsn=momsn,
-            transmit_time=transmit_time,
+            transmit_time=transmit_time_formatted,
             iridium_latitude=iridium_latitude,
             iridium_longitude=iridium_longitude,
             iridium_cep=iridium_cep,
