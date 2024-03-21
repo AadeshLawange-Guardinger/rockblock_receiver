e6bc8a4f02eb4fc23cd42dd557ab3ed0c043a7be
AadeshLawange-Guardinger <aadesh.lawange@guardinger.in>
2024-03-21 17:27:13 +0530

    Data storage done.

--- receiver/decompression.py
+++ receiver/decompression.py
@@ -1,80 +1,140 @@
-import json
-import pywt
-import numpy as np
-import scipy.signal as signal
-import matplotlib.pyplot as plt
-import json
-import soundfile as sf
-import librosa
+# import json
+# import pywt
+# import numpy as np
+# import scipy.signal as signal
+# import matplotlib.pyplot as plt
+# import json
+# import zlib
+
+# # Load the compressed data from the JSON file
+# compressed_file_path = "compressed_data.json"
+
+# with open(compressed_file_path, 'r') as file:
+#     compressed_ascii = json.load(file)
+
+# print("Length of compressed data:", len(compressed_ascii))
+
+# # Convert compressed ASCII back to bytes
+# compressed_data = compressed_ascii.encode('latin-1')
+
+# # Decompress the compressed data
+# decompressed_data = zlib.decompress(compressed_data)
+
+# # Decode the decompressed bytes back to JSON string
+# data_json_decompressed = decompressed_data.decode('utf-8')
+
+# print("Length of decompressed data_json:", len(data_json_decompressed))
+
+# # Convert JSON string back to Python dictionary
+# data_decompressed = json.loads(data_json_decompressed)
+
+
+
+
+# # Retrieve the compressed data
+# comp_cA_ind = np.array(data_decompressed['comp_cA_ind'])
+# comp_cA_val = np.array(data_decompressed['comp_cA_val'])
+# row,col = data_decompressed['cA_thresholded_shape']
+# Zxx_shape = data_decompressed['Zxx_shape']
+
+
+# cA_thresholded_vec_rec =np.zeros(row*col)
+# cA_thresholded_vec_rec[comp_cA_ind]=comp_cA_val
+# cA_thresholded_mat_rec=np.reshape(cA_thresholded_vec_rec,(row,col))
+
+
+# # coeffs_thresholded = (cA_thresholded, (cH_thresholded, cV_thresholded, cD_thresholded))
+# cH_zero = np.zeros((row,col))
+# cV_zero = np.zeros((row,col))
+# cD_zero = np.zeros((row,col))
+# cA_zero = np.zeros((row,col))
+
+# # Reconstructed coefficients
+# coeffs_thresholded = (cA_thresholded_mat_rec, (cH_zero, cV_zero, cD_zero))
+
+# Zxx_compressed = pywt.idwt2(coeffs_thresholded, 'bior1.3')
+
+
+# duration=1
+# re_fs = 3200
+# temp_data=np.ones(int(re_fs*duration))
+
+
+# ff, tt, temp_fft = signal.stft(temp_data, fs=3200, nperseg=512)
+# # Get time and frequency grids
+# T, F = np.meshgrid(tt, ff)
+
+# Zxx_compressed_resized = Zxx_compressed[:Zxx_shape[0], :Zxx_shape[1]]
+
+# # Plot the compressed STFT
+# plt.figure(figsize=(10, 5))
+# plt.pcolormesh(T, F, np.abs(Zxx_compressed_resized), shading='gouraud')
+# plt.title('Reconstructed Signal from Compressed Coefficients')
+# plt.ylabel('Frequency [Hz]')
+# plt.xlabel('Time [sec]')
+# plt.colorbar()
+# plt.tight_layout()
+# plt.show()
+
+
 import zlib
+import json
+import numpy as np
+from scipy import signal
+import pywt
 
-# Load the compressed data from the JSON file
-compressed_file_path = "compressed_data.json"
+def process_compressed_data(data):
+    # Convert compressed ASCII back to bytes
+    compressed_data = data.encode('latin-1')
 
-with open(compressed_file_path, 'r') as file:
-    compressed_ascii = json.load(file)
+    # Decompress the compressed data
+    decompressed_data = zlib.decompress(compressed_data)
 
-print("Length of compressed data:", len(compressed_ascii))
+    # Decode the decompressed bytes back to JSON string
+    data_json_decompressed = decompressed_data.decode('utf-8')
 
-# Convert compressed ASCII back to bytes
-compressed_data = compressed_ascii.encode('latin-1')
+    print("Length of decompressed data_json:", len(data_json_decompressed))
 
-# Decompress the compressed data
-decompressed_data = zlib.decompress(compressed_data)
+    # Convert JSON string back to Python dictionary
+    data_decompressed = json.loads(data_json_decompressed)
 
-# Decode the decompressed bytes back to JSON string
-data_json_decompressed = decompressed_data.decode('utf-8')
+    # Retrieve the compressed data
+    comp_cA_ind = np.array(data_decompressed['comp_cA_ind'])
+    comp_cA_val = np.array(data_decompressed['comp_cA_val'])
+    row, col = data_decompressed['cA_thresholded_shape']
+    Zxx_shape = data_decompressed['Zxx_shape']
 
-print("Length of decompressed data_json:", len(data_json_decompressed))
+    cA_thresholded_vec_rec = np.zeros(row * col)
+    cA_thresholded_vec_rec[comp_cA_ind] = comp_cA_val
+    cA_thresholded_mat_rec = np.reshape(cA_thresholded_vec_rec, (row, col))
 
-# Convert JSON string back to Python dictionary
-data_decompressed = json.loads(data_json_decompressed)
+    # Construct zero arrays for other coefficients
+    cH_zero = np.zeros((row, col))
+    cV_zero = np.zeros((row, col))
+    cD_zero = np.zeros((row, col))
+    cA_zero = np.zeros((row, col))
 
+    # Reconstruct coefficients
+    coeffs_thresholded = (cA_thresholded_mat_rec, (cH_zero, cV_zero, cD_zero))
 
+    # Perform inverse discrete wavelet transform
+    Zxx_compressed = pywt.idwt2(coeffs_thresholded, 'bior1.3')
 
+    # Define parameters for signal generation
+    duration = 1
+    re_fs = 3200
+    temp_data = np.ones(int(re_fs * duration))
 
-# Retrieve the compressed data
-comp_cA_ind = np.array(data_decompressed['comp_cA_ind'])
-comp_cA_val = np.array(data_decompressed['comp_cA_val'])
-row,col = data_decompressed['cA_thresholded_shape']
-Zxx_shape = data_decompressed['Zxx_shape']
+    # Perform Short-Time Fourier Transform (STFT)
+    ff, tt, temp_fft = signal.stft(temp_data, fs=3200, nperseg=512)
 
+    # Get time and frequency grids
+    T, F = np.meshgrid(tt, ff)
 
-cA_thresholded_vec_rec =np.zeros(row*col)
-cA_thresholded_vec_rec[comp_cA_ind]=comp_cA_val
-cA_thresholded_mat_rec=np.reshape(cA_thresholded_vec_rec,(row,col))
+    # Resize the decompressed data to match Zxx_shape
+    Zxx_compressed_resized = Zxx_compressed[:Zxx_shape[0], :Zxx_shape[1]]
 
+    return T, F, Zxx_compressed_resized
 
-# coeffs_thresholded = (cA_thresholded, (cH_thresholded, cV_thresholded, cD_thresholded))
-cH_zero = np.zeros((row,col))
-cV_zero = np.zeros((row,col))
-cD_zero = np.zeros((row,col))
-cA_zero = np.zeros((row,col))
-
-# Reconstructed coefficients
-coeffs_thresholded = (cA_thresholded_mat_rec, (cH_zero, cV_zero, cD_zero))
-
-Zxx_compressed = pywt.idwt2(coeffs_thresholded, 'bior1.3')
-
-
-duration=1
-re_fs = 3200
-temp_data=np.ones(int(re_fs*duration))
-
-
-ff, tt, temp_fft = signal.stft(temp_data, fs=3200, nperseg=512)
-# Get time and frequency grids
-T, F = np.meshgrid(tt, ff)
-
-Zxx_compressed_resized = Zxx_compressed[:Zxx_shape[0], :Zxx_shape[1]]
-
-# Plot the compressed STFT
-plt.figure(figsize=(10, 5))
-plt.pcolormesh(T, F, np.abs(Zxx_compressed_resized), shading='gouraud')
-plt.title('Reconstructed Signal from Compressed Coefficients')
-plt.ylabel('Frequency [Hz]')
-plt.xlabel('Time [sec]')
-plt.colorbar()
-plt.tight_layout()
-plt.show()
-
+# Usage example:
+# T, F, Zxx_compressed_resized = process_compressed_data(unescaped_data)
--- receiver/views.py
+++ receiver/views.py
@@ -1,6 +1,8 @@
 import binascii
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
+
+from receiver.decompression import process_compressed_data
 from .models import RockBlockMessage
 from django.core import serializers
 
@@ -55,4 +57,23 @@ def get_messages(request):
     merged_data = ''.join(messages)
     decoded_data = hex_decoder(merged_data)
     unescaped_data = unescape_unicode(decoded_data)
-    return JsonResponse({'decoded_data': unescaped_data})
+
+    dummy_data = "x\u009c\u00d5\u00d3M\u008a\u00c2@\u0010@\u00e1\u00ab\u0004\u00d7\"]\u0095N\u00aa\u00ca\u009d\u00d7\u0018\u0091 &\u0010\u00c1?\u00c6a\u0010d\u00ee>\u00e9'\u00ea\u0019\u00dc<B\u00a8\u000f\u009a.\u00fa>\u00db\u009d\u008f\u0097n\u00b7\u00ea\u00f6\u00a7~\u00b6\u00ac\u00d6R\u00b7i^M\u0015\u00aa\u00b4\u00a6\u00996\u00b4\u00a5F\u009dF\u00a9a\rkX\u00c3\u001a\u00d6\u00b0\u00865\u00aca\r\u00ebX\u00c7:\u00d6\u00b1\u008eu\u00acc\u001d\u00ebX\u00c7\u00066\u00b0\u00c1|0\u001f\u00cc\u0007\u00f3\u00c1|\u0094\u00f9\u009c\u0012\u0015\u00aa\u00b4\u00a6\u00996\u00b4\u00a5F\u009db\u0005+X\u00c1\nV\u00b0\u0082\u0015\u00ac`\u0005+X\u00c5*V\u00b1\u008aU\u00acb\u0015\u00ab\u00b6\u0099W\u00af\u00e5\u00fcn\u000fe9iQ\u008e\u00f3\u00c9-\u00d7\u00f3\u00ac\u00f2\u0019\u009fQ\u000e\u009b\u00e2QV\u00b3\u00ea~\u00c6\u00ef\u00e1:\u009e\u000f\u00fd\u00d0w\u00d7q{\u0019\u001e\u000fh\u00dan\u009d\u00cb\u00c8\u00d7\u00ed\u00f6\u00fe\u00af\u00cdt\u0001m\u00de\u00fc\u00fd\u0003g\u00e6\u00ac\u007f"
+
+    # Call the function to process the uncompressed data
+    T, F, Zxx_compressed_resized = process_compressed_data(dummy_data)
+
+    # Convert processed data to JSON serializable format
+    T_list = T.tolist()
+    F_list = F.tolist()
+    Zxx_compressed_resized_list = Zxx_compressed_resized.tolist()
+
+    # Send the processed data in the JSON response
+    response_data = {
+        # 'decoded_data': unescaped_data,
+        'T': T_list,
+        'F': F_list,
+        'Zxx_compressed_resized': Zxx_compressed_resized_list
+    }
+
+    return JsonResponse(response_data)
